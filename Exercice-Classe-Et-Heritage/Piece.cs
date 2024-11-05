9ae5c673a1e7231cd6054155b30ada5455d2e1ab
Sarah N <sarahniederits@gmail.com>
2024-11-05 15:19:09 +0100

    Fixed Piece.cs

--- Exercice-Classe-Et-Heritage/Piece.cs
+++ Exercice-Classe-Et-Heritage/Piece.cs
@@ -10,16 +10,15 @@ namespace Exercice_Classe_Et_Heritage
     {
         int Superficie;
         string Nom;
-        public Piece(int superficie, string piece)
+        public Piece(int superficie, string nom)
         {
             Superficie = superficie;
-            Nom = piece;
+            Nom = nom;
         }
 
         public override string ToString()
         {
-            string toString = String.Format("Superficie = {0}m²\n", this.Superficie);
-            toString += String.Format("Nom = {0}\n", this.Nom);
+            string toString = String.Format("La pièce {0} fait {1}m²", this.Nom, this.Superficie);
             return toString;
         }
     }
